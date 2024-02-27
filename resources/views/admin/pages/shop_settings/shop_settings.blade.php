1c687dc94b2c26652c22a84f8455e6d2542e9479
Md Sohel Rana <mdsohelrana77773@gmail.com>
2024-02-27 23:58:45 +0600

    Settings module design

--- resources/views/admin/pages/shop_settings/shop_settings.blade.php
+++ resources/views/admin/pages/shop_settings/shop_settings.blade.php
@@ -1,45 +1,105 @@
 @include('admin.master.header')
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
         <style>
-            .shop-settings-list img{
-                width:50px;
-                height:50px;
+            .shop-settings-list i{
+                font-size: 40px;
+                color: #0090e7;
             }
-            .shop-settings-list .image{
-                margin: 10px 0px 10px 0px;
+            .settings-title strong{
+                font-size: 20px;
             }
-            .shop-settings-list .form-group .image label {
-                vertical-align: bottom;
+            .shop-settings-list .settings-header {
+                display: flex;
+                align-items: center;
+            }
+
+            .shop-settings-list .settings-icon {
+                margin-right: 10px;
+            }
+
+            .shop-settings-list .settings-title {
+                flex: 1;
+            }
+            .search-input-field .form-control, .form-control:focus{
+                border: 1px solid #2A3038;
+                height: calc(2.25rem + 2px);
+                font-weight: normal;
+                font-size: 0.875rem;
+                padding: 0.625rem 0.6875rem;
+                background-color: #2A3038;
+                border-radius: 4px;
+                color: #fff;
             }
         </style>
         <!-- partial -->
         <div class="content-wrapper">
             <div class="row">
                 <x-module-container>
-                    <header>
-                        <h2 class="module-title text-gray-900 dark:text-gray-100">Shop others settings</h2>
-                        <p class="mt-1 text-sm g-color text-green">Here is a list of all the others settings in our store</p>
-                    </header>
-                    <div class="col-md-12 shop-settings-list">
-                        <div class="col-md-4">
-                            <div class="card">
-                                <div class="card-body">
-                                    <h4>Settings title</h4>
-                                    <p>some text</p>
-                                </div>
+                    <div class="row">
+                        <div class="col-md-4 module-text">
+                            <h2 class="module-title text-gray-900 dark:text-gray-100">Shop others settings</h2>
+                            <p class="mt-1 text-sm g-color text-green">Here is a list of all the others settings in our store</p>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="search-input-field">
+                                <input type="text" class="form-control" id="dataSearch" onkeyup="searchFunction()" placeholder="Settings search here...">
                             </div>
                         </div>
-                        <div class="col-md-4">
-                            <div class="card">
-                                <div class="card-body">
-                                    hasldsald
-                                </div>
+                        <div class="col-md-2 product-add text-end">
+                            <div class="dropdown filter-dropdown">
+                                <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">Filter</button>
+                                <ul class="dropdown-menu">
+                                  <li><a class="dropdown-item text-green" href="{{ route('products','1') }}">A to Z</a></li>
+                                  <li><a class="dropdown-item text-yellow" href="{{ route('products','0') }}">Z to A</a></li>
+                                </ul>
                             </div>
                         </div>
-                        <div class="col-md-4">
-                            <div class="card">
+                    </div>
+                    <div class="shop-settings-list">
+                        <div class="row">
+                            <div class="col-md-4 card module-text">
                                 <div class="card-body">
-                                    hasldsald
+                                    <div class="settings-header d-flex">
+                                        <div class="settings-icon">
+                                            <i class="mdi mdi-settings"></i>
+                                        </div>
+                                        <div class="settings-title">
+                                            <strong>Payment Method</strong>
+                                        </div>
+                                    </div>
+                                    <div class="settings-contnt">
+                                        <p>Here is a list of all the others settings in our store</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-4 card module-text">
+                                <div class="card-body">
+                                    <div class="settings-header d-flex">
+                                        <div class="settings-icon">
+                                            <i class="mdi mdi-settings"></i>
+                                        </div>
+                                        <div class="settings-title">
+                                            <strong>Payment Method</strong>
+                                        </div>
+                                    </div>
+                                    <div class="settings-contnt">
+                                        <p>Here is a list of all the others settings in our store</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-4 card module-text">
+                                <div class="card-body">
+                                    <div class="settings-header d-flex">
+                                        <div class="settings-icon">
+                                            <i class="mdi mdi-settings"></i>
+                                        </div>
+                                        <div class="settings-title">
+                                            <strong>Payment Method</strong>
+                                        </div>
+                                    </div>
+                                    <div class="settings-contnt">
+                                        <p>Here is a list of all the others settings in our store</p>
+                                    </div>
                                 </div>
                             </div>
                         </div>
